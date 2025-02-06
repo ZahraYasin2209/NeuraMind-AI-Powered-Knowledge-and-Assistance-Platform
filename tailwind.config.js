984e2fdc9bea6a00e4ee336f3dfe52f7d68df4fc
Zahra Yasin <109916495+ZahraYasin2209@users.noreply.github.com>
2025-02-06 21:27:51 +0500

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class", // Ensure dark mode is manually controlled
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
